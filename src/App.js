c8f81825de17b9eeda9fed20d20ea4eba286043e
Sriganth V <sriganth3@live.com>
2024-05-04 15:34:59 +0530

    Added Navigation for shop component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./routes/home/home.component";
 import Navigation from "./routes/navigation/navigation.component";
 
+
+const Shop = () => {
+  return (
+    <h1>Shop component</h1>
+  )
+}
+
 const App = () => {
   return (
     <Routes>
       <Route path = '/' element = { <Navigation/> }>
         <Route index element = { <Home/> }>
         </Route>
+
+        <Route path="shop" element={<Shop/>}></Route>
+
       </Route>
     </Routes>
   )
