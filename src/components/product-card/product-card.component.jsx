eba34ccbad852277dbc17b0c082306e26b7e3bee
Sriganth V <sriganth3@live.com>
2024-07-19 09:33:18 +0530

    Integrated the shop component display

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,15 +1,18 @@
 import './product-card.styles.scss';
+import Button from "../button/button.component.jsx";
 
 const ProductCard = ({product}) => {
     const {name, price, imageUrl} = product;
-    <div className='product-card-container'>
+   return( 
+   <div className='product-card-container'>
         <img src={imageUrl} alt={`${name}`}></img>
         <div className="footer">
             <span className="name">{name}</span>
             <span className="price">{price}</span>
         </div>
-        <Button buttonType='inverted'>Add to Card</Button>
+        <Button buttonType='inverted'>Add to Cart</Button>
     </div>
+   );
 }
 
 export default ProductCard;
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,15 +1,16 @@
 import SHOP_DATA from '../../shop-data.json';
-
-
+import ProductCard from '../../components/product-card/product-card.component.jsx';
+import { ProductsContext } from '../../contexts/products.context.jsx';
+import { useContext } from 'react';
+import "./shop.styles.scss";
 const Shop = () => {
+
+    const { products } = useContext(ProductsContext);
+
     return (
-        <div>
-            {SHOP_DATA.map(({id, name}) => ( //{ } should not be used here, it's not rendering, not sure why
-                <div key={id}>
-                    <h1>
-                        {name}
-                    </h1>
-                </div>
+        <div className='products-container'>
+            {products.map((product) => ( //{ } should not be used here, it's not rendering, not sure why
+               <ProductCard key={product.id} product={product}/>
             ))}
         </div>
     );
